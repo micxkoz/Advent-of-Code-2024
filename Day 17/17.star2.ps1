575b14e56e4cf9bb6b7e435cf209b8de719ef769
Michał Kozłowski <10565786+mic-koz@users.noreply.github.com>
2024-12-17 23:53:51 +0100

    Change to working solution

--- Day 17/17.star2.ps1
+++ Day 17/17.star2.ps1
@@ -1,20 +1,19 @@
 using namespace System.Collections.Generic
-$input_file = Get-Content $PSScriptRoot\sample2.txt
+$input_file = Get-Content $PSScriptRoot\input.txt
 
 function Get-DebbugerDataFromFile ($File) {
-    $register_A = [int](Select-String -InputObject $input_file -Pattern "Register A: (\d+)").Matches.Groups[1].Value
-    $register_B = [int](Select-String -InputObject $input_file -Pattern "Register B: (\d+)").Matches.Groups[1].Value
-    $register_C = [int](Select-String -InputObject $input_file -Pattern "Register C: (\d+)").Matches.Groups[1].Value
+    $register_A = [long](Select-String -InputObject $input_file -Pattern "Register A: (\d+)").Matches.Groups[1].Value
+    $register_B = [long](Select-String -InputObject $input_file -Pattern "Register B: (\d+)").Matches.Groups[1].Value
+    $register_C = [long](Select-String -InputObject $input_file -Pattern "Register C: (\d+)").Matches.Groups[1].Value
 
     $program = (Select-String -InputObject $input_file -Pattern "Program: ([0-9,]+)").Matches.Groups[1].Value -split ","
 
     return $register_A, $register_B, $register_C, $program
 }
 
-function Get-ProgramOutput ([int]$RegA, [int]$RegB, [int]$RegC, [string[]]$Program) {
+function Get-ProgramOutput ([long]$RegA, [long]$RegB, [long]$RegC, [string[]]$Program) {
     $pointer = 0
     [List[int]]$output = @()
-    $checker = 0
 
     while ($pointer -lt $Program.Count) {
         $opcode = $Program[$pointer]
@@ -29,33 +28,40 @@ function Get-ProgramOutput ([int]$RegA, [int]$RegB, [int]$RegC, [string[]]$Progr
         elseif ($operand -eq 7) {Write-Host "RESERVED - NOT VALID PROGRAM"}
 
 
-        if ($opcode -eq 0) {$RegA = [int][Math]::floor($RegA / [Math]::Pow(2, $combo))}
+        if ($opcode -eq 0) {$RegA = [long][Math]::floor($RegA / [Math]::Pow(2, $combo))}
         elseif ($opcode -eq 1) {$RegB = $RegB -bxor $literal}
         elseif ($opcode -eq 2) {$RegB = $combo % 8}
-        elseif ($opcode -eq 3) {if ($RegA -ne 0) {$pointer = [int]$literal; continue}}
+        elseif ($opcode -eq 3) {if ($RegA -ne 0) {$pointer = [long]$literal; continue}}
         elseif ($opcode -eq 4) {$RegB = $RegB -bxor $RegC}
         elseif ($opcode -eq 5) {$output.Add($combo % 8)}
-        elseif ($opcode -eq 6) {$RegB = [int][Math]::floor($RegA / [Math]::Pow(2, $combo))}
-        elseif ($opcode -eq 7) {$RegC = [int][Math]::floor($RegA / [Math]::Pow(2, $combo))}
-        $pointer += 2
+        elseif ($opcode -eq 6) {$RegB = [long][Math]::floor($RegA / [Math]::Pow(2, $combo))}
+        elseif ($opcode -eq 7) {$RegC = [long][Math]::floor($RegA / [Math]::Pow(2, $combo))}
+        
 
-        $checker = $output.Count
-        if ($checker -gt 0) {
-            if ($Program[$checker-1] -eq $output[$checker-1]) {continue}
-            else {return $false}
-        }
+        $pointer += 2
+        
     }
-    return $($output -join ",") -eq $($Program -join ",")
-    Write-Host "HALT"
+    return $output
 }
+
 
 $registerA, $registerB, $registerC, $program = Get-DebbugerDataFromFile $input_file
 
-$program -join "," | Write-Host
-Write-Host
+$registerA = [Math]::Pow(8, $program.Count-1) + 1
 
-$registerA = [int]$program[0]
-while (-not $(Get-ProgramOutput $registerA $registerB $registerC $program)) {
-    $registerA = $registerA + 8
+while ($true) {
+    $output = Get-ProgramOutput $registerA $registerB $registerC $program
+
+    #Write-Host $($output -join ",")
+    #Write-Host $($program -join ",")
+
+    if ($($output -join ",") -eq $($program -join ",")) {return $registerA}
+    
+    foreach ($i in $($output.Count-1)..0) {
+        if ($output[$i] -ne $program[$i]) {$registerA += [Math]::Pow(8, $i); break}
+    }
 }
-$registerA
+
+# $program -join "," | Write-Host
+# $output = Get-ProgramOutput $registerA $registerB $registerC $program
+# $output -join "," | Write-Host
